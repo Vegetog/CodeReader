9e75612d56ef6fa686b43b9d9257ac69831808b7
Vegetog <2542856720@qq.com>
2025-12-01 16:18:02 +0800

    Initial Commit

---
+++ CodeReader/CodeReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeReaderApp.swift
+//  CodeReader
+//
+//  Created by Vegetog on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CodeReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeReaderTests/CodeReaderTests.swift
@@ -0,0 +1,16 @@
+//
+//  CodeReaderTests.swift
+//  CodeReaderTests
+//
+//  Created by Vegetog on 12/1/25.
+//
+
+import Testing
+
+struct CodeReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
